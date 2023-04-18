6ae42fae06fd23a7b9b870be8d63f592f9885a70
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-04-19 00:49:32 +0300

    Perfect Next Square

---
+++ PerfectSquare.java
@@ -0,0 +1,19 @@
+package Challenges;
+
+public class PerfectSquare {
+
+    public  static void main(String [] args){
+
+        System.out.println(perfectSquare(6));
+        System.out.println(perfectSquare(36));
+        System.out.println(perfectSquare(0));
+        System.out.println(Math.floor(Math.sqrt(-5)));
+
+    }
+
+
+    public static int perfectSquare(int n ){
+        int nextSquare = (int) (Math.floor(Math.sqrt(n)) + 1);
+        return nextSquare* nextSquare;
+    }
+}
