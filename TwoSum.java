c60598bf746c6e2e0bbe9fd078f2bbc0e40d4178
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-07-20 16:19:06 +0300

    Two sum with two pointers

---
+++ TwoSum.java
@@ -0,0 +1,28 @@
+package Challenges;
+
+import java.util.Arrays;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{1,3,4,5}, 6)));
+    }
+    static int [] twoSum(int[]a, int target){
+//        define pointers
+        int left =0, right = a.length -1, i =0;
+
+      while(left < right){
+            int sum = a[left] + a[right];
+          System.out.println(sum);
+            if(sum == target) {
+                return new int[]{ left +1, right +1};
+            } else if (sum < target) {
+                left++;
+            }else {
+                right++;
+            }
+
+      }
+      return new int[]{-1,-1};
+    }
+}
